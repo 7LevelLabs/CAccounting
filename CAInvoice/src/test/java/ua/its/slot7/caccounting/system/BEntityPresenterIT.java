76cffda69734bcd2569154023fdf7c10a7372f63
7LevelLabs <alex.velichko.kyiv@gmail.com>
2014-05-02 13:26:53 +0300

    Opps...

--- CAInvoice/src/test/java/ua/its/slot7/caccounting/system/BEntityPresenterIT.java
+++ CAInvoice/src/test/java/ua/its/slot7/caccounting/system/BEntityPresenterIT.java
@@ -63,6 +63,8 @@ public class BEntityPresenterIT extends Assert {
 		invoice.getInvoicesLines().add(invoiceLine1);
 		invoice.getInvoicesLines().add(invoiceLine2);
 
+		String result = entityPresenter.presentToHTML(invoice);
+
 		assertEquals(entityPresenter.presentToHTML(invoice), "<h3>Invoice 001-001001</h3>\n" +
 			"<p>Preared by : User</p>\n" +
 			"<p>Prepared for : Person</p>\n" +
@@ -70,14 +72,12 @@ public class BEntityPresenterIT extends Assert {
 			"<p>Issue date : Thu Apr 24 17:53:56 EEST 2014</p>\n" +
 			"<p>Payment due date : Thu Apr 24 17:53:56 EEST 2014</p>\n" +
 			"<p><span>Description</span> : <span>Cost</span> : <span>qty</span> : <span>tax</span> : <span>Total</span></p>\n" +
-			"\n" +
 			"<p><span>Line 1</span> : <span>10.00</span> :\n" +
 			"    <span>5</span> : <span>7.00</span> :\n" +
 			"    <span>43.00</span></p>\n" +
 			"<p><span>Line 2</span> : <span>10.00</span> :\n" +
 			"    <span>7</span> : <span>7.00</span> :\n" +
 			"    <span>63.00</span></p>\n" +
-			"\n" +
 			"<p>Subtotal : 106.00</p>\n" +
 			"<p>Discount, % : 10</p>");
 
